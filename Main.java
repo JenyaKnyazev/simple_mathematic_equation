280d89b08d425cc5ad6f442fff26d1aa70ebf65f
Jenya Knyazev <davidknyazev409@gmail.com>
2022-02-26 11:40:03 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.awt.geom.CubicCurve2D;
+import java.util.zip.DataFormatException;
+import java.util.Scanner;
+public class Main {
+    static double calc(double a,char oper,double b){
+        switch(oper){
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+            case '*':
+                return a*b;
+        }
+        return a/b;
+    }
+    static String[] my_split(String s){
+        String res[]=new String[3];
+        boolean flag=false;
+        for(int i=0;i<3;i++)
+            res[i]="";
+        for(int i=0;i<s.length();i++)
+            if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+                res[1]+=s.charAt(i);
+                for(int r=0;r<i;r++)
+                    res[0]+=s.charAt(r);
+                for(int r=i+1;r<s.length();r++)
+                    res[2]+=s.charAt(r);
+                flag=true;
+                break;
+            }
+        if(flag==false){
+            for(int i=0;i<s.length();i++)
+                if(s.charAt(i)=='*' || s.charAt(i)=='/'){
+                    res[1]+=s.charAt(i);
+                    for(int r=0;r<i;r++)
+                        res[0]+=s.charAt(r);
+                    for(int r=i+1;r<s.length();r++)
+                        res[2]+=s.charAt(r);
+                }
+        }
+        return res;
+    }
+    static double recursive_evaluation(String s){
+        double d;
+        try{
+            d = Double.parseDouble(s);
+            return d;
+        }catch(NumberFormatException ex){
+            String run[]=my_split(s);
+            return calc(recursive_evaluation(run[0]),run[1].charAt(0),recursive_evaluation(run[2]));
+        }
+    }
+    public static void main(String []args){
+        String s;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter mathematic equation like 2*2+5/3");
+        s=scan.next();
+        System.out.println("Result: "+recursive_evaluation(s));
+    }
+}
